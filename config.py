460ff6053af29d0cf41b26705393768a4f4aa6c9
Trung Nguyen (Tyler) <37546053+trungnguyen21@users.noreply.github.com>
2024-03-26 22:41:54 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-assemblyai = 'f562cc3c29404695b011d169cac384a6'
-pexelKey = 'H7RQEZFtNV7T9jUQhA4KUeXVkByVnQzER1wdbxi1grI5opadowYykDCR'
-elevenLabsKey = '6b37389ac37023e712f1dca984f4dce9'
+assemblyai = 'ASSEMBLY_API_KEY'
+pexelKey = 'PEXELS_KEY'
+elevenLabsKey = 'ELEVENLABS_KEY'
